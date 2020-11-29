6650e26301df5a7bded50e9d8c83727b3bb54a08
Marcus Chiu <marcuschiu@marcuss-mbp-2.lan>
2020-11-28 20:26:56 -0600

    initial commit

---
+++ src/test/java/com/marcuschiu/apachekafkaexample/ApacheKafkaExampleProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcuschiu.apachekafkaexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheKafkaExampleProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
