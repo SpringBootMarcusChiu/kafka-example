f10e2f009de47ab5c79d23b851dfc355b1a0bdec
Marcus Chiu <marcuschiu9@gmail.com>
2021-08-16 23:07:56 -0500

    lots more stuff

--- src/main/java/com/marcuschiu/apachekafkaexample/config/KafkaAdminConfiguration.java
+++ src/main/java/com/marcuschiu/apachekafkaexample/config/KafkaAdminConfiguration.java
@@ -1,7 +1,6 @@
 package com.marcuschiu.apachekafkaexample.config;
 
 import org.apache.kafka.clients.admin.AdminClientConfig;
-import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,13 +20,5 @@ public class KafkaAdminConfiguration {
         Map<String, Object> configs = new HashMap<>();
         configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         return new KafkaAdmin(configs);
-    }
-
-    @Value(value = "${kafka.topic.name}")
-    private String topicName;
-
-    @Bean
-    public NewTopic topic1() {
-        return new NewTopic(topicName, 1, (short) 1);
     }
 }
